4cec02173295c5d3e770e0412c1d3f8c31b892fa
Nimely111 <samuelehnimely@gmail.com>
2025-05-12 23:29:45 +0000

    initial commit

---
+++ app/_layout.jsx
@@ -0,0 +1,11 @@
+import { Slot, Stack } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const RootLayout = () => {
+  return <Stack />;
+};
+
+export default RootLayout;
+
+const styles = StyleSheet.create({});
