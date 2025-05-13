f6ca0be1ec25dacd1fb6b3ddc6c37811511787cf
Nimely111 <samuelehnimely@gmail.com>
2025-05-13 12:55:55 +0000

    update header UI

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,7 +3,12 @@ import { StyleSheet, Text, View } from "react-native";
 
 const RootLayout = () => {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: "#ddd" },
+        headerTintColor: "#333",
+      }}
+    >
       <Stack.Screen name="index" options={{ title: "Home" }} />
       <Stack.Screen name="about" options={{ title: "About" }} />
       <Stack.Screen
